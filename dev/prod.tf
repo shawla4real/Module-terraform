125bdba50b4cbcc76e58cc249263a55149da0684
shawla4real <65089196+shawla4real@users.noreply.github.com>
2020-10-07 13:00:55 -0400

    Update prod.tf

--- dev/prod.tf
+++ dev/prod.tf
@@ -9,14 +9,14 @@ data "aws_availability_zones" "available" {
                 }
 
 module "solavpc" {
-  source = "/home/ec2-user/terraform-app/modules/vpc"
+  source = "Module-terraform/modules/vpc"
   vpc_cidr = "10.0.0.0/16"
   tenancy = "default"
   vpc_id = "${module.solavpc.vpc_id}"
   subnet_cidr = "10.0.1.0/24"
 }
 module "solaec2" {
-  source = "/home/ec2-user/terraform-app/modules/ec2"
+  source = "Module-terraform/modules/ec2"
   ec2_count = 1
   instance_type = "t2.micro"
   subnet_id = "${module.solavpc.subnet_id}"
